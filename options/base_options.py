a7a0807ea8599201427833df957ac202fb09c5e5
Siyi <syzou01@gmail.com>
2022-04-12 19:40:47 +0800

    change configure opt --model

--- options/base_options.py
+++ options/base_options.py
@@ -25,8 +25,8 @@ class BaseOptions():
         parser.add_argument('--use_wandb', action='store_true', help='use wandb')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--checkpoints_dir', type=str, default='./train_output/ckpt/', help='models are saved here')
-        # model parameters #--model
-        parser.add_argument('--model', type=str, default='pix2pix', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+        # model parameters #--model pix2pix
+        parser.add_argument('--model', type=str, default='colorization', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
         parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
